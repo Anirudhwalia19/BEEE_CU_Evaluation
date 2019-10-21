137c007109082c572ed39669c18e671ded60370b
Anirudhwalia19 <53787402+Anirudhwalia19@users.noreply.github.com>
2019-10-21 23:57:19 +0530

    Add files via upload

---
+++ phase-1 evaluation/anirudh_beeee.ino
@@ -0,0 +1,33 @@
+int LED = 13;
+
+int BUTTON = 2;
+
+int BUTTON_PRESSES = 0;
+
+void setup()
+{
+
+pinMode(13,OUTPUT);
+
+pinMode(2,INPUT);
+
+}
+
+void loop()
+{
+
+if(digitalRead(2) == HIGH)
+{
+digitalWrite(13,HIGH);
+delay(100);
+digitalWrite(13,LOW);
+delay(100);
+}
+else
+{
+
+digitalWrite(13,LOW);
+
+}
+
+}
